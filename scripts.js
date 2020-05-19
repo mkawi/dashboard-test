3382cb1fc9351599d881a9c33c5043af29ebd4bc
Moudi Kawi <kawimoudi@gmail.com>
2020-05-20 00:20:33 +0100

    Add input field limitations

--- scripts.js
+++ scripts.js
@@ -7,10 +7,18 @@ function changeTotal(price, qty) {
 	total.value = (Math.round(total.value * 100) / 100).toFixed(2);
 }
 
-price.addEventListener("change", () => {
+price.addEventListener("keyup", (e) => {
 	changeTotal(price.value, qty.value);
 });
 
-qty.addEventListener("change", () => {
+qty.addEventListener("keyup", () => {
+	qty.value = String(Math.floor(Number(qty.value)));
 	changeTotal(price.value, qty.value);
+});
+
+// Limit number of periods to one
+price.addEventListener("keydown", (e) => {
+	if (price.value.includes(".") && (e.keyCode === 190 || e.keyCode === 110)) {
+		e.preventDefault();
+	}
 });
